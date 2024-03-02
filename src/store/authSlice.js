db391d608e0809005b059718c89e07a01135d22c
swainseemran <swainseemran@gmail.com>
2024-03-02 22:51:44 +0530

    my first react blog

---
+++ src/store/authSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+//it is used for track the authentication process
+//user is authentication is check each and every time to store
+//dont need to spread all the existing value which is present in side state bcz it already implemented in redux toolkit
+
+const initialState = {
+    status: false,
+    userData: null
+}
+
+const authSlice = createSlice({
+    name: "auth", 
+    initialState,
+    reducers: {
+        login: (state, action) =>{
+            state.status = true;
+            state.userData = action.payload.userData;
+        },
+        logout: (state) => {
+            state.status = false;
+            state.userData = null;
+        },
+        // post: (state) =>{
+        //     state.post = true;
+        //     state.userPost = action.payload;
+        // }
+    }
+})
+
+//here actions means login and logout is an action
+export const {login, logout} = authSlice.actions;
+
+export default authSlice.reducer;
