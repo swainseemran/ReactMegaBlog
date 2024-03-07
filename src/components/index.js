f6300d946b5a56e06156b6a8917a1d94a11f1307
swainseemran <swainseemran@gmail.com>
2024-03-07 12:14:47 +0530

    build some pages

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,21 @@ import Footer from "./Footer/Footer";
 import container from './container/Container'
 import Logo from './Logo'
 import LogoutBtn from "./Header/LogoutBtn";
+import Signup from "../pages/Signup";
+import Login from "../pages/Login";
+import Post from "./Post";
+import PostForm from "./post-form/PostForm"
+import PostCard from "./PostCard"
 
 export {
     Header,
     Footer,
     container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Signup,
+    Login,
+    Post,
+    PostForm,
+    PostCard
 }
